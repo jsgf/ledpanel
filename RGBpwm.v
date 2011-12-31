4ea0eb4f24221553a650bb94882b2843c2ed46b2
Jeremy Fitzhardinge <jeremy@goop.org>
2011-12-31 23:13:51 +1100

    failed experiment in making pwm more interesting

--- RGBpwm.v
+++ RGBpwm.v
@@ -1,4 +1,4 @@
-module RGBpwm(input wire [PWM_WIDTH - 1:0] pwm,
+module RGBpwm(input wire [PWM_WIDTH - 1:0] pwmlvl,
 	      input wire [PWM_WIDTH - 1:0] red,
 	      input wire [PWM_WIDTH - 1:0] green,
 	      input wire [PWM_WIDTH - 1:0] blue,
@@ -8,9 +8,19 @@ module RGBpwm(input wire [PWM_WIDTH - 1:0] pwm,
 	      output wire 		   blueled);
 
    parameter PWM_WIDTH = 12;
+   localparam [PWM_WIDTH-1:0] PWM_RANGE = (1<<PWM_WIDTH)-1;
+   localparam [PWM_WIDTH-1:0] HALFRANGE = PWM_RANGE/2;
    
-   assign redled = red > pwm;
-   assign greenled = green > pwm;
-   assign blueled = blue > pwm;
+   function pwm;
+      input [PWM_WIDTH-1:0] 		   lvl, pix;
+      begin
+	 //pwm = ((HALFRANGE - (lvl/2)) < pix) && (pix < (HALFRANGE - (lvl/2) + lvl));
+	 pwm = pix > lvl;
+      end
+   endfunction //
+   
+   assign redled = pwm(pwmlvl, red);
+   assign greenled = pwm(pwmlvl, green);
+   assign blueled = pwm(pwmlvl, blue);
    
 endmodule
